0d5911547b97f8da000a8438b5be1b5f01d2293c
Cemal Gönültaş <cemalgnlts@gmail.com>
2023-04-24 10:33:12 +0300

    index.html file formatted.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,13 +10,13 @@ import NavbarContextProvider from "./context/NavbarContext";
 import LoggerProvider from "./context/LoggerContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <EditorProvider>
-    <NavbarContextProvider>
-      <LoggerProvider>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </LoggerProvider>
-    </NavbarContextProvider>
-  </EditorProvider>
+	<EditorProvider>
+		<NavbarContextProvider>
+			<LoggerProvider>
+				<React.StrictMode>
+					<App />
+				</React.StrictMode>
+			</LoggerProvider>
+		</NavbarContextProvider>
+	</EditorProvider>
 );
